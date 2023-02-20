458cb029ef09ef3da5726d8eba6655c12b304977
gracebir <grace.devolop@gmail.com>
2023-02-20 18:34:07 +0200

    make the navbar responsive

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,49 +1,60 @@
-import React from 'react'
-import { GiHamburgerMenu } from 'react-icons/gi'
-import { GrClose } from 'react-icons/gr'
+import React, {useState} from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { GrClose } from "react-icons/gr";
 
 const navItems = [
   {
-    linkName: 'A propos',
-    href: '#propos'
+    linkName: "A propos",
+    href: "#propos",
   },
   {
-    linkName: 'Nos Activités',
-    href: '#activities'
+    linkName: "Nos Activités",
+    href: "#activities",
   },
   {
-    linkName: 'Contact',
-    href: '#contact'
-  }
-]
+    linkName: "Contact",
+    href: "#contact",
+  },
+];
 
 function NavBar() {
+  const [isToggled, setIsToggle] = useState(false)
+
+  const handleToggle = () => setIsToggle(!isToggled)
   return (
-    <header className="absolute w-full flex justify-between items-center md:max-w-7xl md:mx-auto md:px-16 md:py-6 text-primary">
-    <a className="flex items-center font-semibold" href="#hero">
-        <img className="h-10" src="https://i.ibb.co/YfKbFYG/logo.png" alt=""/>
-        <span className="font-poppins text-2xl tracking-wider">CAPG</span>
-    </a>
-    <div className='md:hidden'>
-      <GiHamburgerMenu size={30}/>
-    </div>
-    <div className="hidden md:flex font-semibold md:gap-8 md:flex-row">
-        {navItems.map((item, i)=> (
-          <a key={i} className="hover:text-lime-700 hover:underline text-[1rem]" href={item.href}>{item.linkName}</a>
-        ))}
-    </div>
-    <div className="md:hidden relative top-0 right-0 w-[90%] h-[50vh] bg-primary flex flex-col font-semibold text-white p-4 md:gap-8 md:flex-row">
-      <div className='text-white'>
-        <GrClose size={30}/>
+    <header className="w-full flex px-4 py-4 h-[10vh] justify-between border z-auto items-center md:max-w-7xl md:mx-auto md:px-16 md:py-10 text-primary">
+      <div className="flex justify-between items-center w-full md:w-fit">
+        <a className="flex items-center font-semibold" href="#hero">
+          <img
+            className="h-10"
+            src="https://i.ibb.co/YfKbFYG/logo.png"
+            alt=""
+          />
+          <span className="font-poppins text-2xl font-bold tracking-wider">
+            CAPG
+          </span>
+        </a>
+        <div onClick={handleToggle} className="md:hidden block cursor-pointer">
+          {isToggled ? <GrClose size={30}/> : <GiHamburgerMenu size={30} />}
+        </div>
       </div>
-      <div className='flex flex-col gap-6 items-center'>
-        {navItems.map((item, i)=> (
-          <a key={i} className="hover:underline text-[1rem]" href={item.href}>{item.linkName}</a>
+
+      <div className={isToggled ?
+        `transition-all opacity-100 ease-in duration-500 flex flex-col top-14 bg-white md:mt-0 w-full md:right-0 md:flex-row md:w-fit px-4 left-0 absolute` :
+        `flex flex-col md:mt-0 md:flex md:gap-7 md:items-center md:static absolute w-full md:flex-row left-0 md:right-0 md:w-fit md:opacity-100 top-[-200px] md:top-0 transition-all opacity-0 ease-in duration-500`
+      }>
+        {navItems.map((item, i) => (
+          <a
+            key={i}
+            className="hover:text-lime-700 hover:underline text-[1rem] py-2 md:py-0"
+            href={item.href}
+          >
+            {item.linkName}
+          </a>
         ))}
       </div>
-    </div>
-</header>
-  )
+    </header>
+  );
 }
 
-export default NavBar
+export default NavBar;
