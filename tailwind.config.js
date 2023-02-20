be28a52ebec83cd9a50419704cd4241f8aede2e4
gracebir <grace.devolop@gmail.com>
2023-02-20 11:03:58 +0200

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      backgroundColor:{
+        bgGreen: '#33a15b'
+      },
+      backgroundImage: {
+        footer: "./images/bg-footer-top-desktop.svg"
+      },
+      colors:{
+        primary: '#417d57'
+      }
+    },
   },
   plugins: [],
 }
